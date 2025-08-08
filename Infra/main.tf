fa9b33431bb7e3815a23cc3dd11088108121e5db
Santosh Singh <santosh1808.singh@gmail.com>
2025-08-08 19:32:51 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "sonu_rg" {
-  name     = "sonu-rg"
+resource "azurerm_resource_group" "sony_rg" {
+  name     = "sony-rg"
   location = "southeastasia"
 }
 
-resource "azurerm_kubernetes_cluster" "sonu_cluster" {
-  name                = "sonu-aks"
+resource "azurerm_kubernetes_cluster" "sony_cluster" {
+  name                = "sony-aks"
   location            = "southeastasia"
-  resource_group_name = azurerm_resource_group.sonu_rg.name
-  dns_prefix          = "sonuaksdns"
+  resource_group_name = azurerm_resource_group.sony_rg.name
+  dns_prefix          = "sonyaksdns"
 
   default_node_pool {
     name       = "default"
@@ -27,16 +27,16 @@ resource "azurerm_kubernetes_cluster" "sonu_cluster" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "SonuACR001" # globally unique
-  resource_group_name = azurerm_resource_group.sonu_rg.name
+  name                = "sonyACR001" # globally unique
+  resource_group_name = azurerm_resource_group.sony_rg.name
   location            = "southeastasia"
   sku                 = "Basic"
   admin_enabled       = true # only for dev/test
 }
 
-resource "azurerm_role_assignment" "sonu_acr_pull" {
+resource "azurerm_role_assignment" "sony_acr_pull" {
   depends_on           = [azurerm_container_registry.acr]
-  principal_id         = azurerm_kubernetes_cluster.sonu_cluster.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.sony_cluster.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
 }
